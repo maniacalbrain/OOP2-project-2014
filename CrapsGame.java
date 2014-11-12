00fbe54a757a3f3aa5fee30dd91b900c42d40f0b
unknown <t00119195@1XX3417EBA7D578.students.ittralee.local>
2014-11-12 15:49:45 +0000

    Created Dice and CrapsGame

---
+++ CrapsGame.java
@@ -0,0 +1,26 @@
+/**
+ * @(#)CrapsGame.java
+ *
+ *
+ * @author 
+ * @version 1.00 2014/11/12
+ */
+
+
+public class CrapsGame extends Dice{
+
+    public CrapsGame() {
+    }
+    //craps -  2, 3, 12 - pass line looses - don't pass win (don't pass are pushed on a 12)
+    //natural - 7, 11 - pass line wins automaticlly, 
+    //point - 4, 5, 6, 8, 9, 10 establishes point
+    
+    //take bets on pass and don't pass
+    //roll = get rolls
+    //if craps end turn
+    //if natural win
+    //if point - roll = point
+    //keep rolling until point is equalled (pass wins) or 7 (pass looses)
+    
+    
+}
---
+++ Dice.java
@@ -0,0 +1,12 @@
+//Dice will return a randomly generated value between 1 and 6
+import java.util.Random;
+
+public class Dice {	
+	
+	public int getRoll(){
+		Random roll = new Random();
+		return roll.nextInt(6)+1;
+	}    
+    
+    
+}
